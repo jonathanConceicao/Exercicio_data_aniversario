e9af40eff34a414ad0da07d40bf133de6ba688a6
jonathanConceicao <145912772+jonathanConceicao@users.noreply.github.com>
2026-01-19 10:33:35 -0300

    Add files via upload

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicacao(num1, num2) {
+    return num1 + num2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(20, 30));
+console.log(saudacao("jonathan"));
+//# sourceMappingURL=exercicio.js.map
---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+
+function multiplicacao(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+console.log(multiplicacao(20, 30)); 
+console.log(saudacao("jonathan")); 
